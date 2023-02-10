5ecad47d96b2eb616ddbd7061ed536c34ca36f61
lsd <lsd@ehk.com.tw>
2023-02-10 15:59:16 +0800

    新增認證middleware

--- JWT-basic/controllers/main.js
+++ JWT-basic/controllers/main.js
@@ -29,12 +29,26 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.headers);
-  const luckyNumber = Math.floor(Math.random() * 100 + 1);
-  res.status(200).json({
-    msg: `Hello, Near Luo`,
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-  });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100 + 1);
+
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401);
+  }
 };
 
 module.exports = {
