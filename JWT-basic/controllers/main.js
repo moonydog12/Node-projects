aeaa8ab6552cd333350228b4be2fb87786c7482e
lsd <lsd@ehk.com.tw>
2023-02-10 16:42:37 +0800

    完成小型會員認證project

--- JWT-basic/controllers/main.js
+++ JWT-basic/controllers/main.js
@@ -3,14 +3,14 @@
 // 傳送 token 到前端
 // 設定驗證機制(authentication)，只有擁有JWT的要求能連線到dashboard
 const jwt = require('jsonwebtoken');
-const CustomAPIError = require('../errors/custom-error');
+const { BadRequestError } = require('../errors/index');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   // 在controller中進行驗證
   if (!username || !password) {
-    throw new CustomAPIError('Emil and password are required,you idiot!', 400);
+    throw new BadRequestError('Emil and password are required,you idiot!');
   }
 
   // 測試功能，通常id會由資料庫端取得
@@ -29,12 +29,10 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  // 從middleware取得資料
-  console.log(req.user);
-
   const luckyNumber = Math.floor(Math.random() * 100 + 1);
 
   res.status(200).json({
+    // 從middleware取得資料
     msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
