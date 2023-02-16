4ceee1cdec1268a02edd8f1f2816283953bf58e1
lsd <lsd@ehk.com.tw>
2023-02-16 10:47:19 +0800

    新增權限限制、API請求上限、聚合資料請求

--- Jobster-API/middleware/authentication.js
+++ Jobster-API/middleware/authentication.js
@@ -1,4 +1,3 @@
-const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const { UnauthenticatedError } = require('../errors');
 
@@ -12,8 +11,11 @@ const auth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name };
+    // 檢查登入帳號是否為測試用
+    const testUser = payload.userId === '63ec7e669f312e2fa848417b';
+    req.user = { userId: payload.userId, name: payload.name, testUser };
     next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid');
--- Jobster-API/routes/jobs.js
+++ Jobster-API/routes/jobs.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const testUser = require('../middleware/testUser');
 
 const router = express.Router();
 const {
@@ -7,10 +8,17 @@ const {
   getAllJobs,
   updateJob,
   getJob,
+  showStats,
 } = require('../controllers/jobs');
 
-router.route('/').post(createJob).get(getAllJobs);
+router.route('/').post(testUser, createJob).get(getAllJobs);
+router.route('/stats').get(showStats);
 
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+// 加上 testUser 來限制測試人員的權限
+router
+  .route('/:id')
+  .get(getJob)
+  .delete(testUser, deleteJob)
+  .patch(testUser, updateJob);
 
 module.exports = router;
