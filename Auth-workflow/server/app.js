6232ce4fc88852106c64667eaabbc095065b1c44
lsd <lsd@ehk.com.tw>
2023-03-09 11:34:48 +0800

    新增Auth-workflow初始專案

--- Auth-workflow/server/app.js
+++ Auth-workflow/server/app.js
@@ -1,18 +1,13 @@
-// 環境變數
 require('dotenv').config();
 require('express-async-errors');
-
 // express
+
 const express = require('express');
-
 const app = express();
-
-// packages
+// rest of the packages
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
-
-// security packages
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
 const xss = require('xss-clean');
@@ -22,39 +17,34 @@ const mongoSanitize = require('express-mongo-sanitize');
 // database
 const connectDB = require('./db/connect');
 
-// routers
+//  routers
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const orderRouter = require('./routes/orderRoutes');
 
-// middleware(會依據順序執行，因此須根據程式邏輯進行排列)
+// middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 60 }));
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan('tiny'));
-app.use(express.json()); // automatically parse incoming json into object
+app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-// 上傳圖片
 app.use(express.static('./public'));
 app.use(fileUpload());
-
-app.get('/', (req, res) => {
-  res.send('e-commerce API');
-});
-
-app.get('/api/v1', (req, res) => {
-  res.send('e-commerce API');
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
@@ -62,14 +52,16 @@ app.use('/api/v1/products', productRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/orders', orderRouter);
 
-app.use(notFoundMiddleware); // 處理無效路由
-app.use(errorHandlerMiddleware); // 處理路由擲出的錯誤
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
   } catch (error) {
     console.log(error);
   }
