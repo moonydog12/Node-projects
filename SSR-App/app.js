10860759bdb7bfaf1c380139587e9bf515fc7ad9
lsd <lsd@ehk.com.tw>
2023-03-14 11:10:48 +0800

    新增樣板處理器(ejs)

--- SSR-App/app.js
+++ SSR-App/app.js
@@ -1,31 +1,45 @@
 const express = require('express');
 
-// 新增express實例
+// Instantiate express object
 const app = express();
 
-// 監聽 requests
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
 app.listen(5000);
 
+app.use(express.static(`${__dirname}/public`));
+
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {
-    root: __dirname,
-  });
+  const blogs = [
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+  ];
+
+  // provide parameters for index.ejs
+  res.render('index', { title: 'Thunder body!', blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', {
-    root: __dirname,
-  });
+  res.render('about', { title: 'About' });
 });
 
-// 重新導向
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
-// 404 pages(若上面的 handler 都不符合，執行這一段)
+// 404 pages(A catch route)
 app.use((req, res) => {
-  res.status(404).sendFile('./views/error.html', {
-    root: __dirname,
-  });
+  res.status(404).render('error', { title: '404' });
 });
